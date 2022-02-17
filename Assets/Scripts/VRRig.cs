1c87315badd0d4cae30889424d6f168ef999d074
pauld <pauld@AW-PAULD-2018-8.drexel.edu>
2022-02-16 21:52:16 -0500

    building virtual space world in oculus quest

---
+++ Assets/Scripts/VRRig.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRRig : MonoBehaviour {
+
+    public Transform touchL;
+    public Transform touchR;
+
+    // Update is called once per frame
+    void Update()
+    {
+        touchL.localPosition = OVRInput.GetLocalControllerPosition(OVRInput.Controller.LTouch);
+        touchR.localPosition = OVRInput.GetLocalControllerPosition(OVRInput.Controller.RTouch);
+
+    }
+}
