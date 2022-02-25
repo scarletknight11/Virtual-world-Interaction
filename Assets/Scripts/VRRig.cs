3a5a678feb237d92b632fde1265012da3befbe65
Sanjit Singh <ss3448@scarletmail.rutgers.edu>
2022-02-25 16:43:58 -0500

    integrated mrtk and got project to build onto quest 2

--- Assets/Scripts/VRRig.cs
+++ Assets/Scripts/VRRig.cs
@@ -13,5 +13,9 @@ public class VRRig : MonoBehaviour {
         touchL.localPosition = OVRInput.GetLocalControllerPosition(OVRInput.Controller.LTouch);
         touchR.localPosition = OVRInput.GetLocalControllerPosition(OVRInput.Controller.RTouch);
 
+        touchL.localRotation = OVRInput.GetLocalControllerRotation(OVRInput.Controller.LTouch);
+        touchR.localRotation = OVRInput.GetLocalControllerRotation(OVRInput.Controller.RTouch);
+
+
     }
 }
